cc7b83f2384113cbfffbd8345d72935bf41d0a92
Ruslan Rozhkov <bladteam@gmail.com>
2016-12-26 17:39:15 +0200

    Added a user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'pages#home'
+
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
