b4a3142147bb30b6ea11850bd93c87ee57105cc0
Ruslan Rozhkov <bladteam@gmail.com>
2016-12-26 21:40:37 +0200

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'coder1', email: 'coder1@gmail.com', password: 'test123', password_confirmation: 'test123')
+User.create(username: 'coder2', email: 'coder2@gmail.com', password: 'test123', password_confirmation: 'test123')
+User.create(username: 'coder3', email: 'coder3@gmail.com', password: 'test123', password_confirmation: 'test123')
+User.create(username: 'coder4', email: 'coder4@gmail.com', password: 'test123', password_confirmation: 'test123')
+User.create(username: 'coder5', email: 'coder5@gmail.com', password: 'test123', password_confirmation: 'test123')
+
+p 'Test users created'
